9a71e7d799a80694b2fb7913de6570cf487f5928
Sarah Vessels <cheshire137@gmail.com>
2018-07-04 14:28:14 -0500

    Add a map select

--- src/components/MatchForm.js
+++ src/components/MatchForm.js
@@ -47,6 +47,44 @@ class MatchForm extends Component {
             />
           </dd>
         </dl>
+        <dl className="form-group">
+          <dt>
+            <label
+              htmlFor="match-map"
+            >Map:</label>
+          </dt>
+          <dd>
+            <select className="form-select">
+              <option value=""></option>
+              <optgroup label="Assault">
+                <option value="Hanamura">Hanamura</option>
+                <option value="Horizon Lunar Colony">Horizon Lunar Colony</option>
+                <option value="Temple of Anubis">Temple of Anubis</option>
+                <option value="Volskaya Industries">Volskaya Industries</option>
+              </optgroup>
+              <optgroup label="Escort">
+                <option value="Dorado">Dorado</option>
+                <option value="Junkertown">Junkertown</option>
+                <option value="Rialto">Rialto</option>
+                <option value="Route 66">Route 66</option>
+                <option value="Watchpoint: Gibraltar">Watchpoint: Gibraltar</option>
+              </optgroup>
+              <optgroup label="Hybrid">
+                <option value="Blizzard World">Blizzard World</option>
+                <option value="Eichenwalde">Eichenwalde</option>
+                <option value="Hollywood">Hollywood</option>
+                <option value="King's Row">King's Row</option>
+                <option value="Numbani">Numbani</option>
+              </optgroup>
+              <optgroup label="Control">
+                <option value="Ilios">Ilios</option>
+                <option value="Lijiang Tower">Lijiang Tower</option>
+                <option value="Nepal">Nepal</option>
+                <option value="Oasis">Oasis</option>
+              </optgroup>
+            </select>
+          </dd>
+        </dl>
         <div className="form-actions">
           <button type="submit" className="btn">Save match</button>
         </div>
