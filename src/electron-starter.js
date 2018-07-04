0730e691cbd54a6143c677c0debf7b40e642fd63
Sarah Vessels <cheshire137@gmail.com>
2018-07-04 10:54:58 -0500

    Need to import path too

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,6 +1,7 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
 const url = require('url')
+const path = require('path')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
