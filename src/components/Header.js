95888dff242b6faca8ee625fd59b3e96e6729b0f
Sarah Vessels <cheshire137@gmail.com>
2018-07-04 10:21:14 -0500

    Replace boilerplate with a header

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Competiwatch Desktop</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
