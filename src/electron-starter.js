440f4e56b0d07d20af716a2eeaaf31564f20e69a
Sarah Vessels <cheshire137@gmail.com>
2018-07-04 10:51:54 -0500

    Import url package for use with non-dev app

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -1,5 +1,6 @@
 // Modules to control application life and create native browser window
 const {app, BrowserWindow} = require('electron')
+const url = require('url')
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
