609d5e58639aa1d47ce7118ff1e6cf6a6eb7d28f
Sarah Vessels <cheshire137@gmail.com>
2018-07-08 09:46:59 -0500

    Open dev tools on app open

--- src/electron-starter.js
+++ src/electron-starter.js
@@ -20,7 +20,7 @@ function createWindow () {
   mainWindow.loadURL(startUrl)
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
