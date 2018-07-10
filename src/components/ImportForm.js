98db6f591d022efc2d075c9dd0fabb4ab1ce03ee
Sarah Vessels <cheshire137@gmail.com>
2018-07-10 17:51:26 -0500

    Get the path of the chosen file on form submit

--- src/components/ImportForm.js
+++ src/components/ImportForm.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react'
 
 class ImportForm extends Component {
+  constructor(props) {
+    super(props)
+    this.state = { path: '' }
+  }
+
   onImport = event => {
     event.preventDefault()
+    console.log(this.state.path)
+  }
+
+  onFileChange = event => {
+    const file = event.target.files[0]
+    if (!file) {
+      return
+    }
+    this.setState(prevState => ({ path: file.path }))
   }
 
   render() {
@@ -12,7 +26,15 @@ class ImportForm extends Component {
       >
         <dl className="form-group mt-0">
           <dt><label htmlFor="csv">Choose a CSV file:</label></dt>
-          <dd><input type="file" id="csv" required className="form-control" /></dd>
+          <dd>
+            <input
+              type="file"
+              id="csv"
+              required
+              className="form-control"
+              onChange={this.onFileChange}
+            />
+          </dd>
         </dl>
         <button type="submit" className="btn btn-primary">
           Import matches
