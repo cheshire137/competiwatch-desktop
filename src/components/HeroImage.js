f0d766efac4b9e3dda056edffc52737c358c32f5
Sarah Vessels <cheshire137@gmail.com>
2018-07-10 19:16:27 -0500

    Don't throw error when showing image for unknown hero

--- src/components/HeroImage.js
+++ src/components/HeroImage.js
@@ -1,10 +1,22 @@
 import React, { Component } from 'react'
 import HeroUtil from '../models/HeroUtil'
 
+const knownHeroes = [
+  'ana', 'bastion', 'brigitte', 'doomfist', 'dva', 'genji',
+  'hanzo', 'junkrat', 'lucio', 'mccree', 'mei', 'mercy', 'moira',
+  'orisa', 'pharah', 'reaper', 'reinhardt', 'roadhog', 'soldier76',
+  'sombra', 'symmetra', 'torbjorn', 'tracer', 'widowmaker', 'winston',
+  'wrecking-ball', 'zarya', 'zenyatta'
+]
+
 class HeroImage extends Component {
   render() {
     const { hero, className } = this.props
     const slug = HeroUtil.slugify(hero)
+    if (knownHeroes.indexOf(slug) < 0) {
+      return <span>{hero}</span>
+    }
+
     const src = require(`../images/heroes/${slug}.png`)
 
     return (
