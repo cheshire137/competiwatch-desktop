d23e5856af9367a0615df4720fdf36655ea6bada
Sarah Vessels <cheshire137@gmail.com>
2018-07-10 19:07:36 -0500

    Map CSV columns to expected match fields

--- src/models/CsvImporter.js
+++ src/models/CsvImporter.js
@@ -1,4 +1,6 @@
 import csv from 'csv'
+import Match from './Match'
+
 const fs = window.require('fs')
 
 class CsvImporter {
@@ -8,7 +10,7 @@ class CsvImporter {
     this.accountID = accountID
   }
 
-  readFile() {
+  readFile = () => {
     return new Promise((resolve, reject) => {
       console.log('reading', this.path)
       fs.readFile(this.path, 'latin1', (err, data) => {
@@ -22,19 +24,70 @@ class CsvImporter {
     })
   }
 
-  import() {
+  parseCsv = () => {
     return new Promise((resolve, reject) => {
       this.readFile().then(lines => {
         const options = { columns: true }
         csv.parse(lines, options, (err, data) => {
           if (err) {
-            console.error('failed to import CSV file', this.path, err)
+            console.error('failed to parse CSV file', this.path, err)
             reject(err)
           } else {
             resolve(data)
           }
         })
       })
+    })
+  }
+
+  normalizeData = hash => {
+    const keys = Object.keys(hash)
+
+    for (const key of keys) {
+      const lowerKey = key.toLowerCase()
+      let value = hash[key]
+
+      if (typeof value === 'string') {
+        const lowerValue = value.toLowerCase()
+
+        if (lowerValue === 'y') {
+          value = true
+        } else if (lowerValue === 'n') {
+          value = false
+        }
+      }
+
+      hash[lowerKey] = value
+    }
+
+    return hash
+  }
+
+  importMatch = rawData => {
+    const data = this.normalizeData(rawData)
+    const matchData = {
+      accountID: this.accountID,
+      season: this.season,
+      rank: data.rank,
+      comment: data.comment,
+      map: data.map,
+      isPlacement: data.placement,
+      result: data.placement ? data.result : null,
+      group: data.group,
+      heroes: data.heroes,
+      day: data.day,
+      time: data.time,
+      enemyThrower: data['enemy thrower'],
+      allyThrower: data['ally thrower'],
+      enemyLeaver: data['enemy leaver'],
+      allyLeaver: data['ally leaver']
+    }
+    return matchData
+  }
+
+  import() {
+    return this.parseCsv().then(rows => {
+      return rows.map(row => this.importMatch(row))
     })
   }
 }
