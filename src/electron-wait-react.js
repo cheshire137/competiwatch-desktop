e3685842f1c6652ef94adb461384c2775ecf3c04
Sarah Vessels <cheshire137@gmail.com>
2018-07-15 15:11:16 -0500

    Pass Electron output to console

--- src/electron-wait-react.js
+++ src/electron-wait-react.js
@@ -1,23 +1,32 @@
-const net = require('net');
-const port = process.env.PORT ? (process.env.PORT - 100) : 3000;
+const net = require('net')
+const port = process.env.PORT ? (process.env.PORT - 100) : 3000
 
-process.env.ELECTRON_START_URL = `http://localhost:${port}`;
+process.env.ELECTRON_START_URL = `http://localhost:${port}`
 
-const client = new net.Socket();
+const client = new net.Socket()
 
-let startedElectron = false;
-const tryConnection = () => client.connect({port: port}, () => {
-  client.end();
-  if(!startedElectron) {
-    console.log('starting electron');
-    startedElectron = true;
-    const exec = require('child_process').exec;
-    exec('npm run electron');
+let startedElectron = false
+const tryConnection = () => client.connect({ port }, () => {
+  client.end()
+
+  if (!startedElectron) {
+    console.log('Starting Electron...')
+    startedElectron = true
+    const { exec } = require('child_process')
+
+    exec('npm run electron', (err, stdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`)
+        return
+      }
+      console.log(`stdout: ${stdout}`)
+      console.log(`stderr: ${stderr}`)
+    })
   }
-});
+})
 
-tryConnection();
+tryConnection()
 
 client.on('error', (error) => {
-  setTimeout(tryConnection, 1000);
-});
+  setTimeout(tryConnection, 1000)
+})
