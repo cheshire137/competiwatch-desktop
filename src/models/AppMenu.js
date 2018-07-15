5e3645cc3dfdb08c4d56165531fc6ae799cc4f3c
Sarah Vessels <cheshire137@gmail.com>
2018-07-14 21:33:00 -0500

    Add bug report menu option, dev tools option

--- src/models/AppMenu.js
+++ src/models/AppMenu.js
@@ -1,10 +1,15 @@
 import os from 'os'
+import PackageInfo from '../../package.json'
+
 const remote = window.require('electron').remote
+const shell = window.require('electron').shell
 const { Menu, app } = remote
 
 class AppMenu {
   constructor(options) {
     this.onPageChange = options.onPageChange
+    this.isMac = os.release().indexOf('Macintosh') > -1
+    this.altOrOption = this.isMac ? 'Option' : 'Alt'
 
     const template = this.getMenuTemplate()
     const menu = Menu.buildFromTemplate(template)
@@ -12,7 +17,7 @@ class AppMenu {
   }
 
   getMenuTemplate() {
-    if (os.release().indexOf('Macintosh') > -1) {
+    if (this.isMac) {
       return this.getMacMenuTemplate()
     }
 
@@ -20,34 +25,79 @@ class AppMenu {
   }
 
   getMacMenuTemplate() {
-    return [{
-      label: app.getName(),
-      submenu: [
-        this.aboutMenuItem(),
-        { type: 'separator' },
-        {
-          label: 'Quit',
-          accelerator: 'Command+Q',
-          click() { app.quit() }
-        }
-      ]
-    }]
+    return [
+      {
+        label: app.getName(),
+        submenu: [
+          this.aboutMenuItem(),
+          { type: 'separator' },
+          {
+            label: 'Quit',
+            accelerator: 'Command+Q',
+            click() { app.quit() }
+          }
+        ]
+      },
+      {
+        label: 'Tools',
+        submenu: [
+          this.developerToolsMenuItem()
+        ]
+      },
+      {
+        label: 'Help',
+        submenu: [
+          this.bugReportMenuItem()
+        ]
+      }
+    ]
   }
 
   getNonMacMenuTemplate() {
-    return [{
-      label: 'Help',
-      submenu: [
-        this.aboutMenuItem()
-      ]
-    }]
+    return [
+      {
+        label: 'Tools',
+        submenu: [
+          this.developerToolsMenuItem()
+        ]
+      },
+      {
+        label: 'Help',
+        submenu: [
+          this.aboutMenuItem(),
+          this.bugReportMenuItem()
+        ]
+      }
+    ]
   }
 
   aboutMenuItem() {
     const self = this
+
     return {
       label: `About ${app.getName()}`,
       click() { self.onPageChange('about') }
+    }
+  }
+
+  bugReportMenuItem() {
+    const self = this
+
+    return {
+      label: 'Report a bug',
+      click() { shell.openExternal(PackageInfo.bugs.url) }
+    }
+  }
+
+  developerToolsMenuItem() {
+    return {
+      label: 'Developer Tools',
+      accelerator: `CmdOrCtrl+${this.altOrOption}+I`,
+      click(item, win) {
+        if (win) {
+          win.webContents.toggleDevTools()
+        }
+      }
     }
   }
 }
