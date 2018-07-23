3af28c19d0237eb375d9fa52fb09c7ae16b8166a
Sarah Vessels <cheshire137@gmail.com>
2018-07-22 21:32:57 -0500

    Add SeasonSelect test

---
+++ src/components/SeasonSelect.test.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import SeasonSelect from './SeasonSelect'
+
+it('renders', () => {
+  const div = document.createElement('div')
+  const latestSeason = 11
+  const activeSeason = 10
+  ReactDOM.render(<SeasonSelect activeSeason={activeSeason} latestSeason={latestSeason} />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
