69d4dfb01e6bc82dfa3833ba1434f5a792cd958a
Sarah Vessels <cheshire137@gmail.com>
2018-07-22 22:01:50 -0500

    Start on AccountSelect tests
    Updates once when promise resolves with the active account lookup, so snapshot is wrong (shows no selected account) but it should be finding the button at least.

---
+++ src/components/AccountSelect.test.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+
+const accounts = [
+  { _id: '456', battletag: 'CheshireCat' },
+  { _id: '123', battletag: 'MarchHare' }
+]
+
+jest.mock('electron', () => ({
+  ipcRenderer: {
+    once: (replyTo, handler) => {
+      handler(null, null, { _id: '123', battletag: 'MarchHare' })
+    },
+    send: () => null
+  }
+}))
+import AccountSelect from './AccountSelect'
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<AccountSelect accounts={accounts} activeAccountID="123" />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('renders', () => {
+  const div = document.createElement('div')
+  const wrapper = shallow(<AccountSelect accounts={accounts} activeAccountID="123" />)
+  const button = wrapper.find('.select-menu-button')
+  expect(button.text()).toBe('MarchHare')
+})
