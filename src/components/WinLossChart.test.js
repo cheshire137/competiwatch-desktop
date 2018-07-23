bb3134e1227914d2787b432ebc7258d53fcdfc22
Sarah Vessels <cheshire137@gmail.com>
2018-07-22 21:30:38 -0500

    Add WinLossChart test

---
+++ src/components/WinLossChart.test.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+jest.mock('react-chartjs-2', () => ({ Pie: () => null }))
+import WinLossChart from './WinLossChart'
+
+it('renders', () => {
+  const div = document.createElement('div')
+  const matches = [
+    { isWin: () => true, isLoss: () => false, isDraw: () => false },
+    { isWin: () => false, isLoss: () => true, isDraw: () => false },
+    { isWin: () => false, isLoss: () => false, isDraw: () => true },
+  ]
+  const season = 11
+  ReactDOM.render(<WinLossChart matches={matches} season={season} />, div)
+  ReactDOM.unmountComponentAtNode(div)
+})
