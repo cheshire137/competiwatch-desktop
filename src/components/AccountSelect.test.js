932c1a9f9130745e51a89985e8876a600ded59bd
Sarah Vessels <cheshire137@gmail.com>
2018-07-28 10:35:41 -0500

    Make AccountSelect render test pass
    Gotta wait for the active account to be looked up and set in the component state.

--- src/components/AccountSelect.test.js
+++ src/components/AccountSelect.test.js
@@ -23,9 +23,36 @@ it('matches snapshot', () => {
   expect(tree).toMatchSnapshot()
 })
 
-it('renders', () => {
+const waitForState = (wrapper, propToBePresent) => {
+  return new Promise((resolve, reject) => {
+    const maxTimeWaited = 2000 // ms
+    const delay = 500 // ms
+    let timeWaited = 0
+    let interval = null
+    const checkState = () => {
+      const state = wrapper.state()
+      if (state[propToBePresent]) {
+        clearInterval(interval)
+        wrapper.update()
+        resolve()
+      } else {
+        timeWaited = timeWaited + delay
+        if (timeWaited > maxTimeWaited) {
+          console.error('waited too long for', propToBePresent, 'in', state)
+          clearInterval(interval)
+          reject()
+        }
+      }
+    }
+
+    interval = setInterval(checkState, delay)
+  })
+}
+
+it('renders', async () => {
   const div = document.createElement('div')
-  const wrapper = shallow(<AccountSelect accounts={accounts} activeAccountID="123" />)
-  const button = wrapper.dive().find('.select-menu-button')
+  const wrapper = shallow(<AccountSelect accounts={accounts} activeAccountID="123" />).dive()
+  await waitForState(wrapper, 'activeAccount')
+  const button = wrapper.find('.select-menu-button')
   expect(button.text()).toBe('MarchHare')
 })
