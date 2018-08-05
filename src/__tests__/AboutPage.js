fe52370ce5f14e0727954759b85ac68ede54fc8b
Sarah Vessels <cheshire137@gmail.com>
2018-08-05 16:21:54 -0500

    Add About page tests

---
+++ src/__tests__/AboutPage.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+
+jest.mock('electron', () => ({
+  remote: {
+    app: {
+      getName: () => 'Sample App'
+    }
+  }
+}))
+import AboutPage from '../components/AboutPage'
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<AboutPage />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
+
+it('displays app name', () => {
+  const div = document.createElement('div')
+  const wrapper = shallow(<AboutPage />)
+  const header = wrapper.find('h1')
+  expect(header.text()).toBe('About Sample App')
+})
