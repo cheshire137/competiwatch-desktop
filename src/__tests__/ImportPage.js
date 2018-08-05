fbb80b756c6a46c369294a53cfe86dceef5eae01
Sarah Vessels <cheshire137@gmail.com>
2018-08-05 16:33:18 -0500

    Add test for warning on import page

---
+++ src/__tests__/ImportPage.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { shallow } from 'enzyme'
+import TestHelpers from '../TestHelpers'
+
+jest.mock('electron', () => ({
+  ipcRenderer: {
+    once: (replyTo, handler) => {
+      if (replyTo.indexOf('find-one') === 0) {
+        handler(null, null, { _id: '123', battletag: 'MarchHare' })
+      } else {
+        const totalMatches = 10
+        handler(null, null, totalMatches)
+      }
+    },
+    send: () => null
+  }
+}))
+import ImportPage from '../components/ImportPage'
+
+it('displays warning when account has matches in that season', async () => {
+  const wrapper = shallow(<ImportPage accountID="123" season="3" />)
+  await TestHelpers.waitForStateProperty(wrapper, 'account')
+  const warnEl = wrapper.find('.flash-warn')
+  expect(warnEl.text()).toMatch(/Importing matches will delete your 10 matches in season 3./)
+})
