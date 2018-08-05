97dbf4dadd8f79684827cca8601a6c88838c5185
Sarah Vessels <cheshire137@gmail.com>
2018-08-05 16:18:04 -0500

    Add Help page render test

---
+++ src/__tests__/HelpPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import HelpPage from '../components/HelpPage'
+
+it('matches snapshot', () => {
+  const tree = renderer.create(<HelpPage />).toJSON()
+  expect(tree).toMatchSnapshot()
+})
