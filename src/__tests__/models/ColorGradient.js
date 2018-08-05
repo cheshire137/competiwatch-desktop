396b574f0c9e9ace16acd7fb3eeb182ebe2f4032
Sarah Vessels <cheshire137@gmail.com>
2018-08-05 16:53:07 -0500

    Add test for ColorGradient

---
+++ src/__tests__/models/ColorGradient.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import renderer from 'react-test-renderer'
+import { shallow } from 'enzyme'
+import ColorGradient from '../../models/ColorGradient'
+
+it('returns an array of RGB colors between given color range', () => {
+  const colors = [[178, 212, 132], [102, 189, 125]]
+  const stepCount = 3
+  const gradient = new ColorGradient(colors, stepCount)
+  const result = gradient.rgb()
+  expect(result.length).toBe(stepCount + 1)
+  expect(result[0]).toEqual(colors[0])
+  expect(result[stepCount]).toEqual(colors[1])
+})
