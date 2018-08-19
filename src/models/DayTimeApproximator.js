a6fa984a259bf2a33b3d08058b5e1fe8ce420eaa
Sarah Vessels <cheshire137@gmail.com>
2018-08-19 13:32:46 -0500

    What if we always use UTC hours?
    Trying to fix failure on Travis still.

--- src/models/DayTimeApproximator.js
+++ src/models/DayTimeApproximator.js
@@ -16,17 +16,17 @@ class DayTimeApproximator {
     if (typeof date === 'string') {
       date = new Date(date)
     }
-    const hours = date.getHours()
+    const hours = date.getUTCHours()
 
-    if (hours >= 5 && hours < 12) {
+    if (hours >= 10 && hours < 17) { // 5a - 11:59a Central
       return 'morning'
     }
 
-    if (hours >= 12 && hours < 17) {
+    if (hours >= 17 && hours < 22) { // 12p - 4:59p Central
       return 'afternoon'
     }
 
-    if (hours >= 17 && hours < 21) {
+    if (hours >= 22 || hours < 2) { // 5p - 8:59p Central
       return 'evening'
     }
 
