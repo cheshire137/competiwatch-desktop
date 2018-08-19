ad038c5ce54d626a16db5406fc58fc9941ea41e0
Sarah Vessels <cheshire137@gmail.com>
2018-08-19 12:56:47 -0500

    Add GithubApi model

---
+++ src/models/GithubApi.js
@@ -0,0 +1,17 @@
+import PackageInfo from '../../package.json'
+
+const baseUrl = 'https://api.github.com'
+
+class GithubApi {
+  async latestVersion() {
+    const repoName = new URL(PackageInfo.repository.url).pathname.replace(/^\//, '')
+    const releasesUrl = `${baseUrl}/repos/${repoName}/releases/latest`
+    const response = await window.fetch(releasesUrl)
+    if (!response.ok) {
+      throw Error(`failed to get latest release: ${response.status} ${response.statusText}`)
+    }
+    const json = await response.json()
+    const { name, html_url } = json
+    return { version: name, url: html_url }
+  }
+}
