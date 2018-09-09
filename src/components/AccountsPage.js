415bba5af2c364a87f80d230c02dccb0f3447f6e
Sarah Vessels <cheshire137@gmail.com>
2018-09-09 15:02:01 -0500

    Add welcome message when no accounts

--- src/components/AccountsPage.js
+++ src/components/AccountsPage.js
@@ -7,35 +7,53 @@ class AccountsPage extends Component {
     const { onAccountChange, onAccountUpdate, season, accounts, onDelete } = this.props
 
     return (
-      <div className="container clearfix layout-children-container">
-        {accounts.length > 0 ? (
-          <div className="float-left col-lg-7 col-md-12 col-sm-12 mb-4">
-            <div className="mr-4 pr-4">
-              <h2
-                className="h2 text-normal mb-2 d-flex flex-items-center"
-              >Battle.net accounts <span className="Counter ml-2 h4 px-2">{accounts.length}</span></h2>
-              <p>Choose an account to view and log competitive matches.</p>
-              <AccountsList
-                season={season}
-                accounts={accounts}
-                onDelete={onDelete}
-                onAccountChange={onAccountChange}
-                onAccountUpdate={onAccountUpdate}
-              />
+      <div className="container layout-children-container">
+        {accounts.length < 1 ? (
+          <div className="Box mb-4 p-3 col-lg-6">
+            <h2
+              className="Subhead-heading mb-2"
+            >
+              <span role="img" className="mr-2 d-inline-block" aria-label="Tada">🎉</span>
+              Welcome to Competiwatch!
+            </h2>
+            <div className="f4">
+              Thanks for using the app! To get started, add a Battle.net
+              account below and log some games.
             </div>
           </div>
         ) : null}
-        <div className="float-left col-lg-5 col-md-12 col-sm-12 mb-4">
-          <h2
-            className="h2 text-normal mb-2"
-          >Add an account</h2>
-          <p>Add an account to log the competitive matches you've played on that account.</p>
-          <div className="pb-3">
-            <AccountForm
-              onCreate={this.props.onCreate}
-              totalAccounts={accounts.length}
-              buttonClass={accounts.length > 0 ? '' : 'btn-primary'}
-            />
+        <div className="clearfix">
+          {accounts.length > 0 ? (
+            <div className="float-left col-lg-7 col-md-12 col-sm-12 mb-4">
+              <div className="mr-4 pr-4">
+                <h2
+                  className="h2 text-normal mb-2 d-flex flex-items-center"
+                >Battle.net accounts <span className="Counter ml-2 h4 px-2">{accounts.length}</span></h2>
+                <p>Choose an account to view and log competitive matches.</p>
+                <AccountsList
+                  season={season}
+                  accounts={accounts}
+                  onDelete={onDelete}
+                  onAccountChange={onAccountChange}
+                  onAccountUpdate={onAccountUpdate}
+                />
+              </div>
+            </div>
+          ) : null}
+          <div className="float-left col-lg-5 col-md-12 col-sm-12 mb-4">
+            <h2
+              className="h2 text-normal mb-2"
+            >{accounts.length > 0 ? 'Add another account' : 'Add an account'}</h2>
+            {accounts.length < 1 ? (
+              <p>Add an account to begin logging competitive matches.</p>
+            ) : null}
+            <div className="pb-3">
+              <AccountForm
+                onCreate={this.props.onCreate}
+                totalAccounts={accounts.length}
+                buttonClass={accounts.length > 0 ? '' : 'btn-primary'}
+              />
+            </div>
           </div>
         </div>
       </div>
