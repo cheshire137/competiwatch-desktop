1d74adc223adb6292f0dcd0a5a4e30f0774ff976
Sarah Vessels <cheshire137@gmail.com>
2019-03-17 17:43:20 -0500

    Tighten up padding on hero headers

--- src/components/HeroSelect.js
+++ src/components/HeroSelect.js
@@ -22,21 +22,21 @@ class HeroSelect extends Component {
     return (
       <div className="d-flex flex-wrap flex-justify-between">
         <div className="hero-box mb-4">
-          <h5 className="h5 border-bottom pb-2 mb-2">Flankers</h5>
+          <h5 className="h5 border-bottom pb-1 mb-2">Flankers</h5>
           <HeroCheckboxList
             heroes={Hero.byType.Flanker}
             isAvailable={this.isAvailable}
             isChecked={this.isChecked}
             onToggle={onToggle}
           />
-          <h5 className="h5 border-bottom pb-2 mt-4 mb-2">Off-tanks</h5>
+          <h5 className="h5 border-bottom pb-1 mt-4 mb-2">Off-tanks</h5>
           <HeroCheckboxList
             heroes={Hero.byType['Off-tank']}
             isAvailable={this.isAvailable}
             isChecked={this.isChecked}
             onToggle={onToggle}
           />
-          <h5 className="h5 border-bottom pb-2 mt-4 mb-2">Off-healers</h5>
+          <h5 className="h5 border-bottom pb-1 mt-4 mb-2">Off-healers</h5>
           <HeroCheckboxList
             heroes={Hero.byType['Off-healer']}
             isAvailable={this.isAvailable}
@@ -45,21 +45,21 @@ class HeroSelect extends Component {
           />
         </div>
         <div className="hero-box mb-4">
-          <h5 className="h5 border-bottom pb-2 mb-2">Hitscan</h5>
+          <h5 className="h5 border-bottom pb-1 mb-2">Hitscan</h5>
           <HeroCheckboxList
             heroes={Hero.byType.Hitscan}
             isAvailable={this.isAvailable}
             isChecked={this.isChecked}
             onToggle={onToggle}
           />
-          <h5 className="h5 border-bottom pb-2 mt-4 mb-2">Main tanks</h5>
+          <h5 className="h5 border-bottom pb-1 mt-4 mb-2">Main tanks</h5>
           <HeroCheckboxList
             heroes={Hero.byType['Main Tank']}
             isAvailable={this.isAvailable}
             isChecked={this.isChecked}
             onToggle={onToggle}
           />
-          <h5 className="h5 border-bottom pb-2 mb-2 mt-4">Main healers</h5>
+          <h5 className="h5 border-bottom pb-1 mb-2 mt-4">Main healers</h5>
           <HeroCheckboxList
             heroes={Hero.byType['Main Healer']}
             isAvailable={this.isAvailable}
@@ -68,14 +68,14 @@ class HeroSelect extends Component {
           />
         </div>
         <div className="hero-box mb-4">
-          <h5 className="h5 border-bottom pb-2 mb-2">DPS</h5>
+          <h5 className="h5 border-bottom pb-1 mb-2">DPS</h5>
           <HeroCheckboxList
             heroes={Hero.byType.DPS}
             isAvailable={this.isAvailable}
             isChecked={this.isChecked}
             onToggle={onToggle}
           />
-          <h5 className="h5 border-bottom pb-2 mt-4 mb-2">Defense</h5>
+          <h5 className="h5 border-bottom pb-1 mt-4 mb-2">Defense</h5>
           <HeroCheckboxList
             heroes={Hero.byType.Defense}
             isAvailable={this.isAvailable}
