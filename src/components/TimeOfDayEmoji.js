242af79de8d1fa6f3177fc851e5bf2e6915b9182
Sarah Vessels <cheshire137@gmail.com>
2019-08-17 11:24:13 -0500

    Convert time of day emoji to func component

--- src/components/TimeOfDayEmoji.js
+++ src/components/TimeOfDayEmoji.js
@@ -1,27 +1,25 @@
 import React, { Component } from 'react'
 
-class TimeOfDayEmoji extends Component {
-  render() {
-    const { timeOfDay } = this.props
+const TimeOfDayEmoji = (props) => {
+  const { timeOfDay } = props
 
-    if (timeOfDay === 'morning') {
-      return <span role="img" aria-label="Morning">🌅</span>
-    }
-
-    if (timeOfDay === 'evening') {
-      return <span role="img" aria-label="Evening">🌆</span>
-    }
-
-    if (timeOfDay === 'afternoon') {
-      return <span role="img" aria-label="Afternoon">😎</span>
-    }
-
-    if (timeOfDay === 'night') {
-      return <span role="img" aria-label="Night">🌝</span>
-    }
-
-    return null
+  if (timeOfDay === 'morning') {
+    return <span role="img" aria-label="Morning">🌅</span>
   }
+
+  if (timeOfDay === 'evening') {
+    return <span role="img" aria-label="Evening">🌆</span>
+  }
+
+  if (timeOfDay === 'afternoon') {
+    return <span role="img" aria-label="Afternoon">😎</span>
+  }
+
+  if (timeOfDay === 'night') {
+    return <span role="img" aria-label="Night">🌝</span>
+  }
+
+  return null
 }
 
 export default TimeOfDayEmoji
