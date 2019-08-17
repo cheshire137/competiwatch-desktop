1d0f52263807288626495bc8040deec9cd4244d5
Sarah Vessels <cheshire137@gmail.com>
2019-08-17 11:17:33 -0500

    Use functional component style for RoleImage

--- src/components/RoleImage.js
+++ src/components/RoleImage.js
@@ -1,30 +1,28 @@
-import React, {Component} from 'react'
+import React from 'react'
 
 const knownRoles = ['tank', 'support', 'damage']
 
-class RoleImage extends Component {
-  render() {
-    const {role, className, size, theme} = this.props
-    const slug = role.toLowerCase()
-    if (knownRoles.indexOf(slug) < 0) {
-      return <span>{role}</span>
-    }
-    let color = ''
-    if (theme === 'dark') {
-      color = '-invert'
-    }
-    const src = require(`../images/roles/${slug}${color}.png`)
-
-    return (
-      <img
-        src={src}
-        alt={role}
-        className={className}
-        width={size || 20}
-        height={size || 20}
-      />
-    )
+const RoleImage = (props) => {
+  const {role, className, size, theme} = props
+  const slug = role.toLowerCase()
+  if (knownRoles.indexOf(slug) < 0) {
+    return <span>{role}</span>
   }
+  let color = ''
+  if (theme === 'dark') {
+    color = '-invert'
+  }
+  const src = require(`../images/roles/${slug}${color}.png`)
+
+  return (
+    <img
+      src={src}
+      alt={role}
+      className={className}
+      width={size || 20}
+      height={size || 20}
+    />
+  )
 }
 
 export default RoleImage
