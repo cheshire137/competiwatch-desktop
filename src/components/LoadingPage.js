d867401f6397b9ba1b958245d0960d29a1ea77b1
Sarah Vessels <cheshire137@gmail.com>
2019-08-17 11:22:18 -0500

    Convert loading page to functional component

--- src/components/LoadingPage.js
+++ src/components/LoadingPage.js
@@ -1,18 +1,16 @@
-import React, { Component } from 'react'
+import React from 'react'
 
-class LoadingPage extends Component {
-  render() {
-    return (
-      <div className="container layout-children-container">
-        <div className="blankslate">
-          <h1>
-            <span className="ion ion-md-refresh mr-3 ion-spin" />
-            Loading...
-          </h1>
-        </div>
+const LoadingPage = () => {
+  return (
+    <div className="container layout-children-container">
+      <div className="blankslate">
+        <h1>
+          <span className="ion ion-md-refresh mr-3 ion-spin" />
+          Loading...
+        </h1>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 export default LoadingPage
