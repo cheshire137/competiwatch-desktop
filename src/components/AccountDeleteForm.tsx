bf24329477e6888ea6cedc2ee36da69fa3089150
Sarah Vessels <cheshire137@gmail.com>
2019-11-16 19:58:42 -0600

    Convert AccountDeleteForm to TypeScript

---
+++ src/components/AccountDeleteForm.tsx
@@ -0,0 +1,44 @@
+import React, { useState } from 'react';
+import Account from '../models/Account';
+
+interface Props {
+  battletag: string;
+  id: string;
+  onDelete: () => {};
+}
+
+const AccountDeleteForm = ({ battletag, id, onDelete }: Props) => {
+  const [showForm, setShowForm] = useState(false);
+  const account = new Account({ _id: id })
+
+  account.hasMatches().then(hasMatches => {
+    if (!hasMatches) {
+      setShowForm(true);
+    }
+  });
+
+  if (!showForm) {
+    return null;
+  }
+
+  const deleteAccount = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+    const message = `Are you sure you want to delete ${battletag}?`
+
+    if (window.confirm(message)) {
+      const account = new Account({ _id: id });
+      account.delete().then(onDelete);
+    }
+  };
+
+  return (
+    <form onSubmit={deleteAccount}>
+      <button
+        type="submit"
+        className="btn-link text-red text-small"
+      >Delete account</button>
+    </form>
+  );
+};
+
+export default AccountDeleteForm;
