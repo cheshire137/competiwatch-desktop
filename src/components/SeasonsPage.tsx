804ba11fbe75695c29b7b1a2c478102f02b56c27
Sarah Vessels <cheshire137@gmail.com>
2019-11-17 11:14:29 -0600

    Convert SeasonsPage to TypeScript

---
+++ src/components/SeasonsPage.tsx
@@ -0,0 +1,43 @@
+import React from 'react';
+import SeasonForm from './SeasonForm';
+import SeasonsList from './SeasonsList';
+
+interface Props {
+  onCreate: (season: number) => void;
+  latestSeason: number;
+  onDelete: (season: number) => void;
+  firstNonDeletableSeason: number;
+  onPageChange: (activePage: string, val1?: any, val2?: any) => void;
+}
+
+const SeasonsPage = ({ latestSeason, onCreate, onPageChange, firstNonDeletableSeason, onDelete }: Props) => (
+  <div className="container layout-children-container">
+    <div className="mt-4">
+      <button
+        type="button"
+        onClick={() => onPageChange('accounts')}
+        className="btn-link"
+      >&larr; Back to your accounts</button>
+    </div>
+    <h1
+      className="h1 mb-2 mt-4"
+    >Manage Seasons</h1>
+    <div className="clearfix">
+      <div className="col-7 float-left">
+        <SeasonsList
+          latestSeason={latestSeason}
+          firstNonDeletableSeason={firstNonDeletableSeason}
+          onDelete={onDelete}
+        />
+      </div>
+      <div className="col-4 float-right">
+        <SeasonForm
+          latestSeason={latestSeason}
+          onCreate={onCreate}
+        />
+      </div>
+    </div>
+  </div>
+);
+
+export default SeasonsPage;
