08927556716e774003d01d2034144adf39ae4094
Sarah Vessels <cheshire137@gmail.com>
2019-11-17 11:12:49 -0600

    Convert SeasonsList to TypeScript

---
+++ src/components/SeasonsList.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import SeasonListItem from './SeasonListItem';
+
+interface Props {
+  latestSeason: number;
+  firstNonDeletableSeason: number;
+  onDelete: (season: number) => void;
+}
+
+const SeasonsList = ({ latestSeason, firstNonDeletableSeason, onDelete }: Props) => {
+  const seasons = Array.from({ length: latestSeason }, (v, k) => k + 1).reverse();
+
+  return (
+    <ul className="list-style-none mb-4">
+      {seasons.map((season, i) => (
+        <SeasonListItem
+          key={season}
+          firstNonDeletableSeason={firstNonDeletableSeason}
+          onDelete={onDelete}
+          seasonNumber={season}
+          index={i}
+        />
+      ))}
+    </ul>
+  );
+};
+
+export default SeasonsList;
