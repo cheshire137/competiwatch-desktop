5ff97063f35d6f8b43c0e59f5d0d93c97e1826b5
Sarah Vessels <cheshire137@gmail.com>
2019-11-17 10:09:18 -0600

    Convert Setting to TypeScript

---
+++ src/models/Setting.ts
@@ -0,0 +1,56 @@
+import Database from './Database';
+
+interface SettingData {
+  _id?: string;
+  defaultAccountID?: string;
+  theme?: string;
+  createdAt?: string;
+}
+
+class Setting {
+  _id?: string;
+  defaultAccountID?: string;
+  theme: string;
+  createdAt?: Date;
+
+  static findAll() {
+    const sort = {};
+    return Database.findAll('settings', sort)
+                   .then(rows => rows.map((data: any) => new Setting(data)));
+  }
+
+  static load() {
+    return this.findAll().then(settings => {
+      return settings[0] || new Setting({});
+    });
+  }
+
+  constructor(data: SettingData) {
+    this._id = data._id;
+    this.defaultAccountID = data.defaultAccountID;
+    this.theme = data.theme || 'light';
+    if (data.createdAt) {
+      this.createdAt = new Date(data.createdAt);
+    }
+  }
+
+  save() {
+    const data = {
+      defaultAccountID: this.defaultAccountID,
+      theme: this.theme
+    };
+    return Database.upsert('settings', data, this._id).then(newSetting => {
+      this._id = newSetting._id;
+      if (newSetting.createdAt) {
+        this.createdAt = newSetting.createdAt;
+      }
+      return this;
+    })
+  }
+
+  delete() {
+    return Database.delete('settings', this._id);
+  }
+}
+
+export default Setting;
