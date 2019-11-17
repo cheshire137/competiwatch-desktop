54529591d09f4eb2a55137eff160068de55c867a
Sarah Vessels <cheshire137@gmail.com>
2019-11-17 11:46:59 -0600

    Convert HeroCheckboxList to TypeScript

---
+++ src/components/HeroCheckboxList.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import HeroCheckbox from './HeroCheckbox'
+import { Hero } from "../models/Hero";
+
+interface Props {
+  heroes: Hero[];
+  isChecked: (hero: Hero) => void;
+  onToggle: (hero: Hero, isSelected: boolean) => void;
+  getUnavailableReason: (hero: Hero) => string | undefined;
+}
+
+const HeroCheckboxList = ({ heroes, getUnavailableReason, isChecked, onToggle }: Props) => (
+  <>
+    {heroes.map(hero => {
+      const unavailableReason = getUnavailableReason(hero)
+      return (
+        <HeroCheckbox
+          key={hero}
+          isAvailable={typeof unavailableReason !== 'string'}
+          unavailableReason={unavailableReason}
+          isChecked={isChecked(hero)}
+          onToggle={onToggle}
+          hero={hero}
+        />
+      )
+    })}
+  </>
+);
+
+export default HeroCheckboxList;
