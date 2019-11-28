06a0f22adc0821d15d895a6acde1b19b4e69ebc8
Sarah Vessels <cheshire137@gmail.com>
2019-11-28 12:14:42 -0600

    Use async/await in SeasonDeleteForm

--- src/components/SeasonDeleteForm.tsx
+++ src/components/SeasonDeleteForm.tsx
@@ -7,18 +7,22 @@ interface Props {
 }
 
 const SeasonDeleteForm = ({ onDelete, seasonNumber }: Props) => {
-  const deleteSeason = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
+  const deleteSeason = async () => {
     const message = `Are you sure you want to delete season ${seasonNumber}? Matches logged in this season will NOT be deleted.`;
-    if (window.confirm(message)) {
-      new Season({ number: seasonNumber })
-        .delete()
-        .then(() => onDelete(seasonNumber));
+
+    if (!window.confirm(message)) {
+      return;
     }
+
+    await new Season({ number: seasonNumber }).delete();
+    onDelete(seasonNumber);
   };
 
   return (
-    <form onSubmit={deleteSeason}>
+    <form onSubmit={evt => {
+      evt.preventDefault();
+      deleteSeason();
+    }}>
       <button type="submit" className="btn-link text-red text-small">
         Delete season
       </button>
