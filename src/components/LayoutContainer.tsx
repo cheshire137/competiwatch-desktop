f67a23d32f0212ebf70cd75de284e756d1b57b6e
Sarah Vessels <cheshire137@gmail.com>
2019-11-29 13:02:42 -0600

    Use Flex in LayoutContainer

--- src/components/LayoutContainer.tsx
+++ src/components/LayoutContainer.tsx
@@ -1,13 +1,14 @@
 import styled from 'styled-components';
+import { Flex } from "@primer/components";
 
 interface Props {
   theme: string;
 }
 
-const LayoutContainer = styled.div<Props>`
-  display: flex;
+const LayoutContainer = styled(Flex).attrs({
+  flexDirection: "column"
+})<Props>`
   min-height: 100vh;
-  flex-direction: column;
   background-color: ${props => props.theme === "dark" ? "#24292e" : "transparent"};
   color: ${props => props.theme === "dark" ? "#e1e4e8" : "inherit"};
 `;
