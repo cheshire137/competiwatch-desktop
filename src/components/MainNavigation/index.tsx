85e06197b3f70273f7dba597c4078c9d2164f65e
Sarah Vessels <cheshire137@gmail.com>
2019-11-29 12:47:15 -0600

    Fix active tab not being highlighted

--- src/components/MainNavigation/index.tsx
+++ src/components/MainNavigation/index.tsx
@@ -15,80 +15,81 @@ interface Props {
   onExport: () => void;
 }
 
-const underlineNavItemClass = (
-  page: string,
-  isButton: boolean,
-  activePage?: string
-): string => {
-  const pageIsFirstInNav = page === "accounts";
-  const activePageIsNotInNav =
-    pageIsFirstInNav && activePage === "manage-seasons";
-  const classes = ["UnderlineNav-item"];
-
-  if (activePage === page || activePageIsNotInNav) {
-    classes.push("selected");
-  }
-
-  if (isButton) {
-    classes.push("btn-link");
-  }
-
-  return classes.join(" ");
-};
-
 const MainNavigation = ({
   activeAccount,
   onPageChange,
   activeSeason,
   activePage,
   onExport
-}: Props) => (
-  <nav className="ml-3 border-0 UnderlineNav width-full d-flex flex-justify-between flex-items-center">
-    <div className="UnderlineNav-body">
-      <AccountsTab
-        onPageChange={onPageChange}
-        activePage={activePage}
-        underlineNavItemClass={underlineNavItemClass}
-      />
-      {activeAccount && (
-        <MatchesTab
+}: Props) => {
+  const underlineNavItemClass = (
+    page: string,
+    isButton: boolean
+  ): string => {
+    const pageIsFirstInNav = page === "accounts";
+    const activePageIsNotInNav =
+      pageIsFirstInNav && activePage === "manage-seasons";
+    const classes = ["UnderlineNav-item"];
+
+    if (activePage === page || activePageIsNotInNav) {
+      classes.push("selected");
+    }
+
+    if (isButton) {
+      classes.push("btn-link");
+    }
+
+    return classes.join(" ");
+  };
+
+  return (
+    <nav className="ml-3 border-0 UnderlineNav width-full d-flex flex-justify-between flex-items-center">
+      <div className="UnderlineNav-body">
+        <AccountsTab
           onPageChange={onPageChange}
           activePage={activePage}
-          activeSeason={activeSeason}
           underlineNavItemClass={underlineNavItemClass}
         />
-      )}
-      <ImportButton
-        activePage={activePage}
-        underlineNavItemClass={underlineNavItemClass}
-      />
-      {activeAccount && (
-        <TrendsTab
+        {activeAccount && (
+          <MatchesTab
+            onPageChange={onPageChange}
+            activePage={activePage}
+            activeSeason={activeSeason}
+            underlineNavItemClass={underlineNavItemClass}
+          />
+        )}
+        <ImportButton
           activePage={activePage}
-          activeAccount={activeAccount}
           underlineNavItemClass={underlineNavItemClass}
-          activeSeason={activeSeason}
-          onPageChange={onPageChange}
         />
-      )}
-    </div>
-    <div>
-      <ExportButton onExport={onExport} activePage={activePage} />
-      {activeAccount && (
-        <LogMatchButton
-          activePage={activePage}
-          activeSeason={activeSeason}
-          onPageChange={onPageChange}
-          activeAccount={activeAccount}
-        />
-      )}
-    </div>
-    {activePage === "log-match" && (
-      <div className="text-gray text-small">
-        * All fields optional except match result
+        {activeAccount && (
+          <TrendsTab
+            activePage={activePage}
+            activeAccount={activeAccount}
+            underlineNavItemClass={underlineNavItemClass}
+            activeSeason={activeSeason}
+            onPageChange={onPageChange}
+          />
+        )}
       </div>
-    )}
-  </nav>
-);
+      <div>
+        <ExportButton onExport={onExport} activePage={activePage} />
+        {activeAccount && (
+          <LogMatchButton
+            activePage={activePage}
+            activeSeason={activeSeason}
+            onPageChange={onPageChange}
+            activeAccount={activeAccount}
+          />
+        )}
+      </div>
+      {activePage === "log-match" && (
+        <div className="text-gray text-small">
+          * All fields optional except match result
+        </div>
+      )}
+    </nav>
+  );
+};
 
 export default MainNavigation;
