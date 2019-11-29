5093cc4197f6330a0d65817ebfe5d441f2914c85
Sarah Vessels <cheshire137@gmail.com>
2019-11-28 19:54:55 -0600

    Use async/await in Season

--- src/models/Season.ts
+++ src/models/Season.ts
@@ -9,15 +9,14 @@ interface SeasonData {
 class Season {
   public static roleQueueSeasonStart: number = 18;
 
-  static latest() {
+  static async latest() {
     const conditions = {};
     const sort = { number: -1 };
 
-    return Database.latest("seasons", conditions, sort).then(data => {
-      if (data) {
-        return new Season(data);
-      }
-    });
+    const data: SeasonData = await Database.latest("seasons", conditions, sort);
+    if (data) {
+      return new Season(data);
+    }
   }
 
   _id?: string;
@@ -42,16 +41,15 @@ class Season {
     return Database.count("matches", { season });
   }
 
-  save() {
+  async save() {
     const data = { number: this.number };
-    return Database.upsert("seasons", data, this._id).then(record => {
-      const newSeason: Season = record as Season;
-      this._id = newSeason._id;
-      if (newSeason.createdAt) {
-        this.createdAt = newSeason.createdAt;
-      }
-      return this;
-    });
+    const record = await Database.upsert("seasons", data, this._id);
+    const newSeason: Season = record as Season;
+    this._id = newSeason._id;
+    if (newSeason.createdAt) {
+      this.createdAt = newSeason.createdAt;
+    }
+    return this;
   }
 
   delete() {
