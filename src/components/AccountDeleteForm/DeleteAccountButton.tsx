94d7c9c3caea10cf31ffa6a85d2786ca5fe5860a
Sarah Vessels <cheshire137@gmail.com>
2020-07-03 13:24:45 -0500

    Fix build error with delete button
    > Property 'theme' does not exist on type 'never'.

--- src/components/AccountDeleteForm/DeleteAccountButton.tsx
+++ src/components/AccountDeleteForm/DeleteAccountButton.tsx
@@ -1,9 +1,27 @@
 import styled from "styled-components";
-import LinkButton from "../LinkButton";
 
-export default styled(LinkButton).attrs({
+export default styled.button.attrs({
   type: "submit"
 })`
+  display: inline-block;
   color: ${props => props.theme.colors.red[5]};
   font-size: 12px;
+  text-decoration: none;
+  white-space: nowrap;
+  cursor: pointer;
+  user-select: none;
+  background-color: transparent;
+  border: 0;
+  background-image: none;
+  font-weight: normal;
+  appearance: none;
+  box-shadow: none;
+
+  &:hover,
+  &:focus {
+    background-image: none;
+    background-color: transparent;
+    text-decoration: underline;
+    box-shadow: none;
+  }
 `;
